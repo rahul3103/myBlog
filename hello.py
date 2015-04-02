6b99f34b01cddf402c4b52bcb40ea1f4cb081eb8
rahul3103 <rahul3103@gmail.com>
2015-04-03 00:00:07 +0530

    Started my project

--- hello.py
+++ hello.py
@@ -1,15 +1,23 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask import request
 app = Flask(__name__)
 
-@app.route('/')
+#@app.route('/')
+#def index():
+#	user_agent = request.headers.get('user_agent')
+#	return '<h1>Your Browser is %s</h1>'%user_agent
+
+#@app.route('/user/<name>')
+#def user(name):
+#	return '<h1>Hello, %s!</h1>' %name
+
+@app.route('/index')
 def index():
-	user_agent = request.headers.get('user_agent')
-	return '<h1>Your Browser is %s</h1>'%user_agent
+	return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-	return '<h1>Hello, %s!</h1>' %name
+	return render_template('user.html',name=name)
 
 if __name__ =='__main__':
 	app.run(debug=True)
