96d875bc3d467f5e8897224267c7d5c5655bb7dd
rahul3103 <rahul3103@gmail.com>
2015-04-30 17:12:13 +0530

    Commit 3
    Completed DB with redis

--- routes.py
+++ routes.py
@@ -1,8 +1,13 @@
 from flask import Flask, url_for, request, render_template;
 from app import app;
+import redis;
+
+# Connect to redis data store
+r = redis.StrictRedis(host='localhost',port=6379, db=0, charset="utf-8", decode_responses=True);
 
 @app.route('/')
 def hello():
+
 	createLink = "<a href = '" + url_for('create') + "'>Create a question</a>";
 	return """<html>
 				<head>
@@ -25,6 +30,13 @@ def create():
 		question = request.form['question'];
 		
 		# Store data in data base
+		# key name will be whatever title they typerd in : Question		
+		#e.g. music:question countries:question
+
+		r.set(title +':question',question)
+		r.set(title +':answer',answer)
+
+
 		return render_template('CreatedQuestion.html', question = question);
 	else:
 		return "<h2>Invalid request</h2>";
@@ -35,18 +47,19 @@ def question (title):
 	if request.method == 'GET':
 		# send the user the form
 
-		question = 'Question here';
+		question = r.get(title+':question');
 		#Read question from data here
+
 		return render_template('AnswerQuestion.html', question = question);
 	elif request.method == 'POST':
 		# User has attempted answer. Check if they are correct
 		submittedAnswer = request.form['submittedAnswer'];
 		
 		# Read answer from data store
-		answer = 'Answer';
+		answer = r.get(title+':answer');
 
 		if submittedAnswer == answer:
-			return  render_template(Correct.html);
+			return  render_template('Correct.html');
 		else:
 			return render_template('Incorrect.html',submittedAnswer = submittedAnswer, answer = answer);
 	else:
